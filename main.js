ebd7d95ea441afecf18e3b9adfaf682cd39de501
guill <guilleloscertales375@gmail.com>
2023-09-02 20:26:51 +0200

    Code cleaning and coments

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 var AutoClicker = {}
 
 AutoClicker.init = function() {
-
-
+  //Init values for menus
   AutoClicker.OffColor = '#b52f18';
   AutoClicker.OnColor = '#63943e';
   AutoClicker.ButtonContainerColor = 'rgba(175, 143, 0, 0.2)';
@@ -14,39 +13,43 @@ AutoClicker.init = function() {
 
   AutoClicker.MenuHidden = true;
 
-
+  //Function to hide or show the menu
   AutoClicker.MenuHideShow = function() {
       if(AutoClicker.MenuHidden) {
-      AutoClicker.ShowContainer.style.visibility = "hidden";    
-      AutoClicker.ButtonContainer.style.visibility = "visible";
-      AutoClicker.MenuHidden = false;
+        AutoClicker.ShowContainer.style.visibility = "hidden";    
+        AutoClicker.ButtonContainer.style.visibility = "visible";
+        AutoClicker.MenuHidden = false;
       }
       else {
-      AutoClicker.ShowContainer.style.visibility = "visible";    
-      AutoClicker.ButtonContainer.style.visibility = "hidden";
-      AutoClicker.MenuHidden = true;
+        AutoClicker.ShowContainer.style.visibility = "visible";    
+        AutoClicker.ButtonContainer.style.visibility = "hidden";
+        AutoClicker.MenuHidden = true;
       }
   };
 
+  //AutoClick the cookie
   AutoClicker.ClickCookie = function() {
+    //if not autoClicker selected in menu, do nothing
     if(!AutoClicker.AutoClickMode) return;
+
     var cookie = document.getElementById('bigCookie');
     AutoClicker.dispatchEvent(cookie, 'click');
+
   };
 
+  //AutoClick the goolden Cookies
   AutoClicker.ClickGoldenCookie = function() {
     if(!AutoClicker.GoldenClickMode) return;
-    var goldenCookie = document.getElementById('shimmers').firstChild;
 
-    //Check if is a goldenCookie and its not wrath, the click it
+    //Check if is a goldenCookie and its not wrath, then click it
     for (var i in Game.shimmers){
       if(Game.shimmers[i].type=='golden' && !Game.shimmers[i].wrath ){
-        //Game.Notify(`golden!`,'',[16,5]);
         AutoClicker.dispatchEvent(Game.shimmers[i].l, 'click');
       } 
     }
   };
 
+  //AutoClick fortunes
   AutoClicker.ClickFortune = function() {
     if(!AutoClicker.FortuneClickMode) return;
     if(Game.TickerEffect && Game.TickerEffect.type === 'fortune') {
@@ -64,9 +67,11 @@ AutoClicker.init = function() {
 
   AutoClicker.setButtonStatus = function(button, status) {
     var statusTextEl = button.getElementsByClassName('status-text')[0];
+
     if(!statusTextEl) return;
+
     statusTextEl.style.backgroundColor = status ? AutoClicker.OnColor : AutoClicker.OffColor;
-      statusTextEl.innerHTML = status ? 'on' : 'off';
+    statusTextEl.innerHTML = status ? 'on' : 'off';
   }
 
   AutoClicker.AutoClickCallback = function(event) {
@@ -89,6 +94,7 @@ AutoClicker.init = function() {
     var statusTextEl = document.createElement('p');
     textEl.innerHTML = text;
     textEl.style.margin = 'auto 0';
+
     //textEl.style.flexBasis = '60%';
     //textEl.style.flexBasis = '45%'
     statusTextEl.innerHTML = 'off';
@@ -98,6 +104,7 @@ AutoClicker.init = function() {
     statusTextEl.style.padding = '0.5rem';
     //statusTextEl.style.textAlign = 'center';
     statusTextEl.style.flexBasis = '20%'
+    
     button.style.display = 'flex';
     button.style.padding = '0.5rem';
     button.style.height = '3.25rem';
@@ -144,20 +151,27 @@ AutoClicker.init = function() {
     container.style.color = 'white';
     container.style.zIndex = '9999';
     container.style.border = '1px solid lightgray';
+
     var autoButton = document.createElement('div');
     AutoClicker.setButtonProps(autoButton, 'Auto Click', AutoClicker.AutoClickCallback);
+    
     var autoGoldenButton = document.createElement('div');
     AutoClicker.setButtonProps(autoGoldenButton, 'Auto Golden', AutoClicker.GoldenAutoClickCallback);
+    
     var autoFortuneButton = document.createElement('div');
     AutoClicker.setButtonProps(autoFortuneButton, 'Auto Fortunes', AutoClicker.FortuneAutoClickCallback);
+    
     var hideButton = document.createElement('div');
     AutoClicker.setButtonHide(hideButton, 'Hide', AutoClicker.MenuHideShow);
+    
     container.appendChild(autoButton);
     container.appendChild(autoGoldenButton);
     container.appendChild(autoFortuneButton);
     container.appendChild(hideButton);
+    
     var gameContainer = document.getElementById('sectionLeft')
     gameContainer.appendChild(container);
+    
     AutoClicker.ButtonContainer = container;
     AutoClicker.AutoButton = autoButton;
     AutoClicker.AutoGoldenButton = autoGoldenButton;
@@ -171,6 +185,7 @@ AutoClicker.init = function() {
     var textEl = document.createElement('p');
     textEl.innerHTML = 'Show';
     textEl.style.margin = 'auto 0';
+    
     var container = document.createElement('div');
     container.style.padding = '1rem';
     container.style.backgroundColor = AutoClicker.ButtonBackgroundColor;
@@ -186,10 +201,14 @@ AutoClicker.init = function() {
     container.addEventListener('click', AutoClicker.MenuHideShow);
     container.appendChild(textEl);
     container.style.cursor = 'pointer';
+    
     AutoClicker.ShowContainer = container;
+    AutoClicker.ShowContainer.style.visibility = "visible";
+    
     var gameContainer = document.getElementById('sectionLeft')
     gameContainer.appendChild(container);
-    AutoClicker.ShowContainer.style.visibility = "visible";
+    
+    
   };
 
   window.setTimeout(() => {
